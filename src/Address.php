87dba49320f21a4ddcdc62278ec628c5edd5bc96
David Rosenbloom <darosenbloom@gmail.com>
2024-01-01 16:10:11 -0500

    support a user defined prefix.

--- src/Address.php
+++ src/Address.php
@@ -4,13 +4,24 @@ namespace kornrunner\Ethereum;
 
 use InvalidArgumentException;
 use kornrunner\Keccak;
-use Mdanter\Ecc\EccFactory;
 use Mdanter\Ecc\Crypto\Key\PrivateKeyInterface;
+use Mdanter\Ecc\EccFactory;
 use Mdanter\Ecc\Serializer\PublicKey\DerPublicKeySerializer;
 
-class Address {
+class Address
+{
 
-    public function __construct(string $privateKey = '') {
+    private const SIZE = 64;
+    protected $prefix = '';
+    /**
+     * @var PrivateKeyInterface
+     */
+    private $privateKey;
+
+    public function __construct(string $privateKey = '', string $prefix = '')
+    {
+        $this->setPrefix($prefix);
+        $privateKey = $this->removePrefix($privateKey);
         $generator = EccFactory::getSecgCurves()->generator256k1();
         if (empty ($privateKey)) {
             $this->privateKey = $generator->createPrivateKey();
@@ -27,25 +38,34 @@ class Address {
         }
     }
 
-    public function getPrivateKey(): string {
-        return str_pad(gmp_strval($this->privateKey->getSecret(), 16), self::SIZE, '0', STR_PAD_LEFT);
+    public function setPrefix(string $prefix = '')
+    {
+        $this->prefix = $prefix;
     }
 
-    public function getPublicKey(): string {
+    public function removePrefix(string $any)
+    {
+        if (substr($any, 0, strlen($this->prefix)) === $this->prefix) {
+            return substr($any, strlen($this->prefix));
+        }
+        return $any;
+    }
+
+    public function getPrivateKey(): string
+    {
+        return $this->prefix . str_pad(gmp_strval($this->privateKey->getSecret(), 16), self::SIZE, '0', STR_PAD_LEFT);
+    }
+
+    public function get(): string
+    {
+        $hash = Keccak::hash(hex2bin($this->removePrefix($this->getPublicKey())), 256);
+        return $this->prefix . substr($hash, -40);
+    }
+
+    public function getPublicKey(): string
+    {
         $publicKey = $this->privateKey->getPublicKey();
         $publicKeySerializer = new DerPublicKeySerializer(EccFactory::getAdapter());
-        return substr($publicKeySerializer->getUncompressedKey($publicKey), 2);
+        return $this->prefix . substr($publicKeySerializer->getUncompressedKey($publicKey), 2);
     }
-
-    public function get(): string {
-        $hash = Keccak::hash(hex2bin($this->getPublicKey()), 256);
-        return substr($hash, -40);
-    }
-
-    /**
-     * @var PrivateKeyInterface
-     */
-    private $privateKey;
-
-    private const SIZE = 64;
 }
